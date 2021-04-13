fa715a45dc99185c5309b2f6bf45f6e79f4d1729
VergaraC <victorvaac@al.insper.edu.br>
2021-04-13 18:27:41 -0300

    Cavera Capitao

--- aps2.py
+++ aps2.py
@@ -13,12 +13,14 @@ delta_x = 0.5
 delta_y = 0.5
 
 #constantes
-k = 1.2
+k = 1
 alpha = 1
 u = alpha
 a = 9/1.4
 b = 60/(9+5)
-
+checar = 0
+C_checar = np.zeros((41,61))
+T_checar = 0
 
 
 for l in tempo:
@@ -49,19 +51,38 @@ for l in tempo:
                 C_[j][0] = C_[j][1]
             elif i == 60:
                 C_[j][60] = C_[j][59]
+            if  not checar:
+                if C_[j][60]!=0 :
+                    print("j",j,i, l)
+                    print(C_[j][0])
+                    T_checar = l
+                    C_checar = np.copy(C_)
+                    checar = 1
+                elif C_[j][0]!=0:
+                    print("j",j,i, l)
+                    print("Tempo: ", l)
+                    print(C_[j][0])
+                    T_checar = l
+                    C_checar = np.copy(C_)
+                    checar = 1
+                elif C_[0][i]!=0:
+                    print("j",j,i, l)
+                    print(C_[j][0])
+                    T_checar = l
+                    C_checar = np.copy(C_)
+                    checar = 1
+                elif C_[40][i]!=0:
+                    print("j",j,i, l)
+                    print(C_[j][0])
+                    T_checar = l
+                    C_checar = np.copy(C_)
+                    checar = 1
 
-    '''#Condições de contorno
-    C_[1:40][0] = C_[1:40][1]
-    C_[1:40][60] = C_[1:40][59]
-    C_[0][1:60] = C_[1][1:60]
-    C_[40][1:60] = C_[39][1:60]'''
-    
-            
     C = np.copy(C_)
 #print(np.shape(C))
 #extent=(0,40,0,60)
 
-plt.imshow(C, vmin = 0, vmax = 1, extent=(0,60,0,40))
+plt.imshow(C_checar, vmin = 0, vmax = 1, extent=(0,60,0,40))
 plt.xlim(0,59)
 plt.ylim(0,40)
 plt.colorbar()
